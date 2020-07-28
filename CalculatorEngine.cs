f945d90c85af446abf614b3b8c0b8084201abde7
adityapatel9619 <55842340+adityapatel9619@users.noreply.github.com>
2020-07-28 21:25:01 +0530

    3

---
+++ CalculatorEngine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace SimpleCalculator
+{
+   public class CalculatorEngine
+    {
+        public double Calculate(string argOperation,double firstNumber,double secondNumber)
+        {
+            double result;
+            switch (argOperation.ToLower())
+            {
+                //For Addition
+                case "add":
+                case "+":
+                    result = firstNumber + secondNumber;
+                    break;
+
+                //For Subtraction
+                case "subtract":
+                case "-":
+                    result = firstNumber - secondNumber;
+                    break;
+
+                //For Multiplication
+                case "multiply":
+                case "*":
+                    result = firstNumber * secondNumber;
+                    break;
+
+                //For Division
+                case "divide":
+                case "/":
+                    result = firstNumber / secondNumber;
+                    break;
+
+                //For Invalid Input Operation
+                default:
+                    throw new InvalidOperationException("Specified Operation Not Recognized");
+            }
+            return result;
+        }
+    }
+}
