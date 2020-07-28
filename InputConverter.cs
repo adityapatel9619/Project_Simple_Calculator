9e561a559ee8642bef52f74d5111e6c025ee7a0c
adityapatel9619 <55842340+adityapatel9619@users.noreply.github.com>
2020-07-28 21:27:00 +0530

    5

---
+++ InputConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace SimpleCalculator
+{
+    public class InputConverter
+    {
+        public double ConvertInputToNumber(string argTextInput)
+        {
+            double convertednumber;
+
+            if(!double.TryParse(argTextInput,out convertednumber))
+            {
+
+                throw new ArgumentException("Expected Numeric Number");
+            }
+            return convertednumber;
+        }
+    }
+}
